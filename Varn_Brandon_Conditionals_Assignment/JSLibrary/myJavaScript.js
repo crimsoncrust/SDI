8e9e4775591773299c66372aa28935bdb55e0ab0
crimsoncrust <brandon_varn@yahoo.com>
2015-02-19 15:59:30 -0600

    Code finished starting flowchart woo...

--- Varn_Brandon_Conditionals_Assignment/JSLibrary/myJavaScript.js
+++ Varn_Brandon_Conditionals_Assignment/JSLibrary/myJavaScript.js
@@ -16,7 +16,7 @@ var enterDungeon = true;
 var correctLvlClass;
 var informationArray;
 
-console.log ("Hello, today we are going to see if you qualify to enter the dungeon!");
+console.log ("Hello, today we are going to see if you qualify to enter the dungeon, if you are lucky enough you might be great enough to get bonus gold apon entering!");
 
 //alert to see if they want to enter dungeon
 
@@ -42,9 +42,9 @@ correctLvlClass = confirm ("Is this information correct? Hero's name is " + hero
 
 //Check to make sure the level is ok.
 
-(heroLevel >= 20)? console.log ("Your Hero's does have a high enough level.  You can enter the dungeon."): console.log ("Your level is not high enough to enter the dungeon.");
+(heroLevel >= 20)? console.log ("Your Hero does have a high enough level."): console.log ("Your level is not high enough to enter the dungeon.");
 
-//sticking in array for easier use
+//sticking in array for easier use// decided not to use it as it was not mandatory in the rubric for a array
 
 informationArray = [heroName, heroClass, heroStr, heroInt, heroWis, heroLevel];
 
@@ -60,10 +60,18 @@ if ((Number(heroClass) == 2 && heroStr > 20)) {
 	console.log ("Your hero is very wise and can enter the dungeon!");
 
 } else {
-   console.log ("Your hero is to weak, stupid, and unwise to enter the dungeon!");
+   console.log ("But your hero is to weak, stupid, and unwise to enter the dungeon!");
 }
 
+//calculations used division and math as well as prompt verification with number
 
+console.log ("Let's see if your hero qualifies for the bonus gold apon entrance of the dungeon!");
+
+if (((Number(heroLevel) + (Number(heroInt) + (Number(heroStr) + (Number (heroWis) / 3) >= 33))))) {
+console.log ("Your hero has gone through many trials and may receive the bonus 100 gold blessing apon successful entrance to the dungeon.");
+} else {
+console.log ("Your hero has not garnered high enough power to recieve the gold blessing.  You may enter the dungeon but no gold bonus for you!");
+}
 
 
 
